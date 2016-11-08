fb3e11e59c330116c7b81917005f1191590e0ff6
zhouhang <2501213087@qq.com>
2016-11-08 20:52:17 +0800

    complete the base framework

--- RadarView/app/src/main/java/com/onektower/radarview/RadarView.java
+++ RadarView/app/src/main/java/com/onektower/radarview/RadarView.java
@@ -3,6 +3,9 @@ package com.onektower.radarview;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,20 +24,32 @@ public class RadarView extends View {
     private static final int defaultLayerNum = 6;
     //多边形默认半径
     private final float defaultRadius;
-    //多边形默认边距
-    private final float defaultEdgeDis;
     //多边形默认线宽
     private final float defaultLineWidth;
+    //多边形默认颜色
+    private final int defaultLineColor;
     //多边形边数
     private int edgeNum;
     //多边形最小半径
     private float radius;
-    //多边形边距
-    private float edgeDis;
     //多边形线宽
     private float lineWidth;
     //层数
     private int layerNum;
+    //颜色
+    private int lineColor;
+    //控件的宽
+    private int width;
+    //控件的高
+    private int height;
+    //画笔
+    private Paint paint;
+    //多边形内角
+    private float angle;
+    //边距
+    private float dis;
+    //是否是奇数多边形
+    private boolean isOdd;
 
     public RadarView(Context context) {
         this(context, null);
@@ -46,20 +61,33 @@ public class RadarView extends View {
 
     public RadarView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        defaultRadius = dp2px(100);
-        defaultEdgeDis = dp2px(100);
-        defaultLineWidth = dp2px(5);
+        defaultRadius = dp2px(200);
+        defaultLineWidth = 2;
+        defaultLineColor = Color.BLACK;
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.RadarViewStyle, defStyleAttr, 0);
         edgeNum = typedArray.getInt(R.styleable.RadarViewStyle_edge_num, defaultEdgeNum);
         radius = typedArray.getDimension(R.styleable.RadarViewStyle_radius, defaultRadius);
-        edgeDis = typedArray.getDimension(R.styleable.RadarViewStyle_edge_dis, defaultEdgeDis);
         lineWidth = typedArray.getDimension(R.styleable.RadarViewStyle_line_width, defaultLineWidth);
+        lineColor = typedArray.getColor(R.styleable.RadarViewStyle_line_color, defaultLineColor);
+        layerNum = typedArray.getInt(R.styleable.RadarViewStyle_layer_num, defaultLayerNum);
         typedArray.recycle();
+
+        angle = (float) ((Math.PI * 2) / edgeNum);
+        isOdd = edgeNum % 2 == 0 ? false : true;
+        initPaint();
+    }
+
+    private void initPaint() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(lineColor);
+        paint.setStrokeWidth(lineWidth);
+        paint.setStyle(Paint.Style.STROKE);
     }
 
     /**
      * 这里是确定控件的大小的方法
+     *
      * @param widthMeasureSpec
      * @param heightMeasureSpec
      */
@@ -70,26 +98,26 @@ public class RadarView extends View {
 
     @Override
     protected int getSuggestedMinimumWidth() {
-        return (int) (defaultEdgeDis*(defaultEdgeNum - 1) + defaultLineWidth*defaultEdgeNum + defaultRadius);
+        return (int) radius;
     }
 
     @Override
     protected int getSuggestedMinimumHeight() {
-        return (int) (defaultEdgeDis*(defaultEdgeNum - 1) + defaultLineWidth*defaultEdgeNum + defaultRadius);
+        return (int) radius;
     }
 
-    private int measure(int measureSpec, boolean isWidth){
+    private int measure(int measureSpec, boolean isWidth) {
         int result;
         int size = MeasureSpec.getSize(measureSpec);
         int mode = MeasureSpec.getMode(measureSpec);
         int padding = isWidth ? getPaddingLeft() + getPaddingRight() : getPaddingTop() + getPaddingBottom();
-        if (mode == MeasureSpec.EXACTLY){//表示用户设定了大小
+        if (mode == MeasureSpec.EXACTLY) {//表示用户设定了大小
             result = size;
         } else {
             result = isWidth ? getSuggestedMinimumWidth() : getSuggestedMinimumHeight();
             result += padding;
-            if (mode == MeasureSpec.AT_MOST){
-                result = Math.max(result, size);
+            if (mode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, size);
             }
         }
         return result;
@@ -97,6 +125,7 @@ public class RadarView extends View {
 
     /**
      * 这里可以得到控件的最终大小
+     *
      * @param w
      * @param h
      * @param oldw
@@ -105,11 +134,42 @@ public class RadarView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
+        dis = width > height ? height / 2 / layerNum - lineWidth : width / 2 / layerNum - lineWidth;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.translate(width / 2, height / 2);
+        Path path = new Path();
+        float currentRadius = dis;
+        for (int i = 0; i < layerNum; i++) {
+            for (int j = 0; j < edgeNum; j++) {
+                float cos = (float) Math.cos(angle * j);
+                float sin = (float) Math.sin(angle * j);
+                float positionX = isOdd ? currentRadius * sin : currentRadius * cos;
+                float positionY = isOdd ? currentRadius * cos : currentRadius * sin;
+                if (j == 0)
+                    path.moveTo(positionX, positionY);
+                else {
+                    path.lineTo(positionX, positionY);
+                }
+            }
+            currentRadius += dis;
+            path.close();
+        }
+        for (int i = 0; i < edgeNum; i++){
+            float realRadius = dis * layerNum;
+            float cos = (float) Math.cos(angle * i);
+            float sin = (float) Math.sin(angle * i);
+            float positionX = isOdd ? realRadius * sin : realRadius * cos;
+            float positionY = isOdd ? realRadius * cos : realRadius * sin;
+            path.moveTo(0, 0);
+            path.lineTo(positionX, positionY);
+        }
+        canvas.drawPath(path, paint);
     }
 
     private float dp2px(float dp) {
